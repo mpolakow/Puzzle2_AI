9c4bd84ec61989b1dd8e8c37b9cf861f230b99a4
Maciej <maciej.polakowski@gmail.com>
2026-04-01 18:44:43 +0200

    Update monster_constants.js

--- monster_constants.js
+++ monster_constants.js
@@ -1,14 +1,16 @@
 const MONSTERS = {
     'goblin': {
-        icon: '♙', type: window.PIECE_TYPES.MONSTER, moveStrategy: 'pawn',
+        icon: 'â™™', type: window.PIECE_TYPES.MONSTER, moveStrategy: 'pawn',
+	image: 'Sprites/Goblin.png',
         Health: 30, Mana: 0, Attack: 10, Attack_Type: window.Attack_Type.MELEE, Attack_Range: 1, Defense: 5, Armor_Type: window.Armor_Type.NONE, Move: 1
     },
     'orc': {
-        icon: '♖', type: window.PIECE_TYPES.MONSTER, moveStrategy: 'rook', range: 4,
+        icon: 'â™–', type: window.PIECE_TYPES.MONSTER, moveStrategy: 'rook', range: 4,
+	image: 'Sprites/Orc.png',
         Health: 80, Mana: 0, Attack: 15, Attack_Type: window.Attack_Type.MELEE, Attack_Range: 1, Defense: 10, Armor_Type: window.Armor_Type.MEDIUM, Move: 1
     },
     'ogre': {
-        icon: '♚', type: window.PIECE_TYPES.MONSTER, moveStrategy: 'king',
+        icon: 'â™š', type: window.PIECE_TYPES.MONSTER, moveStrategy: 'king',
         Health: 150, Mana: 0, Attack: 30, Attack_Type: window.Attack_Type.MELEE, Attack_Range: 1, Defense: 20, Armor_Type: window.Armor_Type.HARD, Move: 1
     }
 };
