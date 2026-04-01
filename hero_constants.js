7f9c289a6715d3bfc1312d25157d7845cb9ea036
Maciej <maciej.polakowski@gmail.com>
2026-04-01 18:44:21 +0200

    Update hero_constants.js

--- hero_constants.js
+++ hero_constants.js
@@ -1,14 +1,16 @@
 const HEROES = {
     'knight': {
-        icon: '♘', type: window.PIECE_TYPES.HERO, moveStrategy: 'knight',
+        icon: 'â™', type: window.PIECE_TYPES.HERO, moveStrategy: 'knight',
+	image: 'Sprites/Knight.png', // <-- Add this
         Health: 100, Mana: 0, Attack: 20, Attack_Type: window.Attack_Type.MELEE, Attack_Range: 1, Defense: 10, Armor_Type: window.Armor_Type.MEDIUM, Move: 1
     },
     'archer': {
-        icon: '♗', type: window.PIECE_TYPES.HERO, moveStrategy: 'bishop', range: 3,
-        Health: 70, Mana: 20, Attack: 15, Attack_Type: window.Attack_Type.RANGED, Attack_Range: 3, Defense: 5, Armor_Type: window.Armor_Type.LIGHT, Move: 1
+        icon: 'â™—', type: window.PIECE_TYPES.HERO, moveStrategy: 'bishop', range: 3,
+	image: 'Sprites/Archer.png',
+        Health: 70, Mana: 20, Attack: 35, Attack_Type: window.Attack_Type.RANGED, Attack_Range: 3, Defense: 5, Armor_Type: window.Armor_Type.LIGHT, Move: 1
     },
     'warrior': {
-        icon: '♔', type: window.PIECE_TYPES.HERO, moveStrategy: 'king',
+        icon: 'â™”', type: window.PIECE_TYPES.HERO, moveStrategy: 'king',
         Health: 120, Mana: 0, Attack: 25, Attack_Type: window.Attack_Type.MELEE, Attack_Range: 1, Defense: 15, Armor_Type: window.Armor_Type.HARD, Move: 1
     }
 };
